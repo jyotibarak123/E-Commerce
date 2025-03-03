924073769c20ee1444272c95be4b9b33ff158194
jyoti barak <jyotibarak02@gmail.com>
2025-03-03 12:31:07 +0530

    commit 1

--- 05_Student_ERP/src/main/java/com/ducat/controllers/CartController.java
+++ 05_Student_ERP/src/main/java/com/ducat/controllers/CartController.java
@@ -27,7 +27,7 @@ public class CartController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		PrintWriter print = response.getWriter();
 		
 		
 		
